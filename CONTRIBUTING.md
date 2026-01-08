22656c53fae985e7b6b100a4ea1d327209f7a7f4
Camp <12837401+campfred@users.noreply.github.com>
2026-01-08 09:23:44 -0500

    Add missing import statements and stuff

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,4 +1,4 @@
-# How to contribute
+# Contributing
 
 Oh hey! Thanks for checking my project out!
 I'll be honest, this was meant more as a way for me to track my Blender projects' renders through my Home Assistant server. But I do love improving stuff!
@@ -6,10 +6,8 @@ If you have ideas, of course feel free to bring them over. I'll be happy to chec
 
 ## Submitting changes
 
-Please open [a pull request to this repository](https://github.com/campfred/blender-render-webhooks-addon/pull/new/main) with a clear list of what you've done.
-I'd love that if you also include examples of the results from your change so that I understand better the impact of the changes.
-If you also give the context for your changes (what prompted to add a feature for example), that would be awesome too!
-
+Please open a pull request to this repository with a clear description of what you've done and the reasoning behind it. Just so that I can understand what happened or what's going on.
+I'd love that if you could also include examples of the results from your change so that I understand better the impact of the changes.
 
 Thanks!
 Camp
--- render_webhooks.py
+++ render_webhooks.py
@@ -1,7 +1,9 @@
+from __future__ import annotations
+
 bl_info = {
     "name": "Render events webhooks",
     "author": "campfred",
-    "version": (1, 0),
+    "version": (1, 1),
     "blender": (2, 93, 0),
     "location": "Render",
     "description": "Send webhooks to a server upon render events.",
@@ -16,6 +18,7 @@ import urllib.request
 import urllib.error
 import bpy
 
+
 ###
 # Utilities
 class RenderEvents(Enum):
@@ -25,54 +28,73 @@ class RenderEvents(Enum):
     CANCEL = "cancel"
     ERROR = "error"
 
+
 def poke_webhook(event: RenderEvents, data: dict[str, str]) -> None:
     # Getting info on what webhook server to poke
     addon_prefs = bpy.context.preferences.addons[__name__].preferences
-    base_url = addon_prefs["webhook_url"]
+    base_url = addon_prefs.webhook_url
     path = getattr(addon_prefs, f"render_{event.value}_path")
     url = f"{base_url}{path}"
-    
+
     frame_first = bpy.context.scene.frame_start
     frame_current = bpy.context.scene.frame_current
     frame_last = bpy.context.scene.frame_end
-    frame_count = frame_last - frame_first + 1 # Using frame_start in case the first frame isn't #0
+    frame_count = (
+        frame_last - frame_first + 1
+    )  # Using frame_start in case the first frame isn't #0
 
     # Prepping basic data about the render job
     job_data = {
         "project_name": bpy.path.basename(bpy.data.filepath),
-        "frame_count": frame_count
+        "output_name": bpy.path.basename(bpy.context.scene.render.filepath),
+        "frame_count": frame_count,
     }
     # Prepping extra data about the render job's progress
     if event == RenderEvents.PROGRESS:
         job_data = job_data | {
             "progress": {
-                "percent": int((frame_current - frame_first) / (frame_last - frame_first) * 100),
+                "percent": int(
+                    (frame_current - frame_first) / (frame_last - frame_first) * 100
+                ),
                 "frames": {
                     "index_first": frame_first,
-                    "index_last" : frame_last,
-                    "index_current": frame_current 
-                }
+                    "index_last": frame_last,
+                    "index_current": frame_current,
+                },
             }
         }
     # Adding the caller's extra data about the render job
     job_data = job_data | data
-    
+
     try:
-        print(f"📨 Sending « PUT » request to {base_url}{path} with data : ", job_data)
-        request = urllib.request.Request(url=url, data=json.dumps(job_data).encode("utf-8"), method="PUT")
+        print(
+            f"Render event webhooks: Sending « PUT » request to {base_url}{path}. 📨 Data : ",
+            job_data,
+        )
+        request = urllib.request.Request(
+            url=url, data=json.dumps(job_data).encode("utf-8"), method="PUT"
+        )
         request.add_header("Content-Type", "application/json")
         with urllib.request.urlopen(request) as response:
             if response.status == 200:
-                print(f"🎉 Server notified succesfully of a render {event.value} event.")
+                print(
+                    f"Render event webhooks: Server notified succesfully of a render {event.value} event. 🎉"
+                )
             else:
-                print(f"❗ Webhook request failed with status code {response.status}.")
-                print("   Response message : ", {response.read().decode("utf-8")})
+                print(
+                    f"Render event webhooks: Webhook request failed with status code {response.status}. ❗ Response message : ",
+                    {response.read().decode("utf-8")},
+                )
     except urllib.error.HTTPError as error:
-        print(f"❗ Webhook request failed with code {error.code}")
-        print("   Response message : ", {error.read().decode("utf-8")})
+        print(
+            f"Render event webhooks: Webhook request failed with code {error.code}. ❗ Response message : ",
+            {error.read().decode("utf-8")},
+        )
     except Exception as exception:
-        print("❗ Error making the webhook requestion : ", {exception})
-
+        print(
+            "Render event webhooks: Error making the webhook request, exception happened. ❗ Exception : ",
+            {exception},
+        )
 
 
 ###
@@ -83,38 +105,38 @@ class RenderWebhookAddonPreferences(bpy.types.AddonPreferences):
     webhook_url: bpy.props.StringProperty(
         name="Webhook URL",
         description="The base URL of the webhook to call.",
-        default="https://server.local/webhook"
-    ) # type: ignore
+        default="https://server.local/api/webhook",
+    )  # type: ignore
 
     render_start_path: bpy.props.StringProperty(
         name="Render start path",
         description="The path to use for the webhook call when the render starts.",
-        default="/render_start"
-    ) # type: ignore
-    
+        default="/render_start",
+    )  # type: ignore
+
     render_progress_path: bpy.props.StringProperty(
         name="Render progress path",
         description="The path to use for the webhook call when the render progresses.",
-        default="/render_progress"
-    ) # type: ignore
+        default="/render_progress",
+    )  # type: ignore
 
     render_complete_path: bpy.props.StringProperty(
         name="Render complete path",
         description="The path to use for the webhook call when the render completes.",
-        default="/render_complete"
-    ) # type: ignore
+        default="/render_complete",
+    )  # type: ignore
 
     render_cancel_path: bpy.props.StringProperty(
         name="Render cancel path",
         description="The path to use for the webhook call when the render cancels.",
-        default="/render_cancel"
-    ) # type: ignore
+        default="/render_cancel",
+    )  # type: ignore
 
     render_error_path: bpy.props.StringProperty(
         name="Render error path",
         description="The path to use for the webhook call when the render errors.",
-        default="/render_error"
-    ) # type: ignore
+        default="/render_error",
+    )  # type: ignore
 
     def draw(self, context):
         layout = self.layout
@@ -126,47 +148,50 @@ class RenderWebhookAddonPreferences(bpy.types.AddonPreferences):
 
 ###
 # Add-on actual actions
-class RenderWebhookAddon(bpy.types.Operator):
-    """Send render events to a webhook"""
-    bl_idname = "object.addon_render_webhook"
-    bl_label = "Render events webhooks"
-    
-    def render_start_callback(self):
-        """Update webhook about render start."""
-        poke_webhook(event=RenderEvents.START, data={})
+def render_start_handler(scene):
+    """Update webhook about render start."""
+    poke_webhook(event=RenderEvents.START, data={})
 
-    def render_progress_callback(self):
-        """Update webhook about render progression."""
-        poke_webhook(event=RenderEvents.PROGRESS, data={})
 
-    def render_complete_callback(self):
-        """Update webhook about render completion."""
-        poke_webhook(event=RenderEvents.COMPLETE, data={})
+def render_progress_handler(scene):
+    """Update webhook about render progression."""
+    poke_webhook(event=RenderEvents.PROGRESS, data={})
 
-    def render_cancel_callback(self, scene):
-        """Update webhook about render cancellation."""
-        # Get the error message from the Blender scene
-        if scene.render.error_message:
-            error_message = scene.render.error_message
-            poke_webhook(event=RenderEvents.ERROR, data={"error_message": error_message})
-        else:
-            poke_webhook(event=RenderEvents.CANCEL, data={})
+
+def render_complete_handler(scene):
+    """Update webhook about render completion."""
+    poke_webhook(event=RenderEvents.COMPLETE, data={})
+
+
+def render_cancel_handler(scene):
+    """Update webhook about render cancellation."""
+    # Get the error message from the Blender scene
+    if scene.render.error_message:
+        error_message = scene.render.error_message
+        poke_webhook(event=RenderEvents.ERROR, data={"error_message": error_message})
+    else:
+        poke_webhook(event=RenderEvents.CANCEL, data={})
 
 
 ###
 # Add-on registration
 def register():
     bpy.utils.register_class(RenderWebhookAddonPreferences)
-    bpy.utils.register_class(RenderWebhookAddon)
-    bpy.app.handlers.render_init.append(RenderWebhookAddon.render_start_callback)
-    bpy.app.handlers.render_write.append(RenderWebhookAddon.render_progress_callback)
-    bpy.app.handlers.render_complete.append(RenderWebhookAddon.render_complete_callback)
-    bpy.app.handlers.render_cancel.append(RenderWebhookAddon.render_cancel_callback)
+    bpy.app.handlers.render_init.append(render_start_handler)
+    bpy.app.handlers.render_write.append(render_progress_handler)
+    bpy.app.handlers.render_complete.append(render_complete_handler)
+    bpy.app.handlers.render_cancel.append(render_cancel_handler)
+    print("Render event webhooks: Registered. ✅ Bonjour!")
+
 
 def unregister():
     bpy.utils.unregister_class(RenderWebhookAddonPreferences)
-    bpy.utils.unregister_class(RenderWebhookAddon)
-    bpy.app.handlers.render_init.remove(RenderWebhookAddon.render_start_callback)
-    bpy.app.handlers.render_write.remove(RenderWebhookAddon.render_progress_callback)
-    bpy.app.handlers.render_complete.remove(RenderWebhookAddon.render_complete_callback)
-    bpy.app.handlers.render_cancel.remove(RenderWebhookAddon.render_cancel_callback)
+    bpy.app.handlers.render_init.remove(render_start_handler)
+    bpy.app.handlers.render_write.remove(render_progress_handler)
+    bpy.app.handlers.render_complete.remove(render_complete_handler)
+    bpy.app.handlers.render_cancel.remove(render_cancel_handler)
+    print("Render event webhooks: Deregistered. ✅ Goodbye!")
+
+
+if __name__ == "__main__":
+    register()
